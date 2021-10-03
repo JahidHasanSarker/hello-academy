b52ac62816f9baf4afafd1080a66a72ec5ab2a74
jahid11223 <jahidhasan1695@gmail.com>
2021-10-04 00:20:15 +0600

    Start

---
+++ src/Component/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+    return (
+        <div>
+            <h1>This Is About Page</h1>
+        </div>
+    );
+};
+
+export default About;
